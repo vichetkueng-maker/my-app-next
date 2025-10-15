d272514a5f860d857c0278f272871b7d3eff59d1
M1 <m1pro@M1s-MacBook-Pro.local>
2025-10-15 18:25:16 +0700

    first commit

---
+++ src/components/layout/user-nav.tsx
@@ -0,0 +1,68 @@
+'use client';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger
+} from '@/components/ui/dropdown-menu';
+import { UserAvatarProfile } from '@/components/user-avatar-profile';
+// import { SignOutButton, useUser } from '@clerk/nextjs';
+import { useRouter } from 'next/navigation';
+export function UserNav() {
+//   const { user } = useUser();
+  const router = useRouter();
+//   if (user) {
+    
+//   }
+return (
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant='ghost' className='relative h-8 w-8 rounded-full'>
+            <UserAvatarProfile user={{
+                imageUrl:"avatar.jpg",
+                fullName:"Kueng vichet",
+                emailAddresses:[
+                    {
+                        emailAddress:"kuengvichet@gmil.com"
+                    }
+                ]
+            }} />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+          className='w-56'
+          align='end'
+          sideOffset={10}
+          forceMount
+        >
+          <DropdownMenuLabel className='font-normal'>
+            <div className='flex flex-col space-y-1'>
+              <p className='text-sm leading-none font-medium'>
+                Kueng vichet
+              </p>
+              <p className='text-muted-foreground text-xs leading-none'>
+                kuengvichet@gmail.com
+              </p>
+            </div>
+          </DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuGroup>
+            <DropdownMenuItem onClick={() => router.push('/dashboard/profile')}>
+              Profile
+            </DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Settings</DropdownMenuItem>
+            <DropdownMenuItem>New Team</DropdownMenuItem>
+          </DropdownMenuGroup>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>
+            Log out
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+    );
+}
